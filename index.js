739d3fc281b985951ec11960f4fa743b604184d8
JavaGamer009 <xurshid@gmail.com>
2025-07-12 15:51:21 +0500

    Men Bu terni yasadim

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+
+
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
@@ -13,6 +15,24 @@ app.get('/', (req, res) => {
 app.get('/support', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', "support.html"))
 });
+
+// Bu yerdan boshlab Modellar qo'yilgan
+
+app.get('/M8', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'M8.html'))
+})
+
+app.get('/M5', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'M5.html'))
+})
+
+app.get('/M4', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'M4.html'))
+})
+
+app.get('/M3', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'M3.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`${PORT} port is running`)
