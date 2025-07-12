0d6fa68c795ee26872d3ac0c61f589a181b2b610
Blackmena <javohiruser2008@gmail.com>
2025-07-12 14:33:48 +0500

    fuck this commit

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', "index.html"))
 })
 
+app.get('/support', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', "support.html"))
+});
+
 app.listen(PORT, () => {
     console.log(`${PORT} port is running`)
 })
